78590f7da45926a9f919bf5b1c99195faaac3e57
Sunhaloo <azmaan151003@gmail.com>
2024-11-01 21:45:05 +0400

    Update OneDark Pro Theme
    I was having trouble reading the comments with the blur transparent
    background with certain wallpapers. Hence, I updated changed the gray
    colour to the light gray colour

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -22,6 +22,10 @@ return {
                     -- strings = 'italic',
                     -- variables = 'bold'
                 },
+				colors = {
+					-- sets the colours of comments to be more readable
+					grey = "#7a818e",
+				},
                 diagnostics = {
                     darker = true,
                     undercurl = true
@@ -47,40 +51,66 @@ return {
     },
     -- the goat @devaslife theme
     {
-      "craftzdog/solarized-osaka.nvim",
-      lazy = false,
-      priority = 1000,
-      config =  function()
-          require("solarized-osaka").setup({
-              -- set the background to transparent
-              transparent = true,
-          })
-       end,
+		"craftzdog/solarized-osaka.nvim",
+		lazy = false,
+		priority = 1000,
+		config =  function()
+		  require("solarized-osaka").setup({
+			  -- set the background to transparent
+			  transparent = true,
+		  })
+		end,
     },
     -- eldritch theme
     {
-      "eldritch-theme/eldritch.nvim",
-      lazy = false,
-      priority = 1000,
-      config = function()
-          require("eldritch").setup({
-              terminal_colors = true,
-              transparent = true,
-          })
-      end,
+		"eldritch-theme/eldritch.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+		  require("eldritch").setup({
+			  terminal_colors = true,
+			  transparent = true,
+		  })
+		end,
     },
     -- cyberdream theme
     {
-    "scottmckendry/cyberdream.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require("cyberdream").setup({
-            terminal_colors = true,
-            transparent = true,
-            italic_comments = true,
-            borderless_telescope = true,
-        })
-    end,
+		"scottmckendry/cyberdream.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("cyberdream").setup({
+				terminal_colors = true,
+				transparent = true,
+				italic_comments = true,
+				borderless_telescope = true,
+			})
+		end,
     },
+	-- flow theme
+	{
+		"0xstepit/flow.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("flow").setup({
+				-- enable the dark theme
+				dark_theme = true,
+				-- make background transparent
+				transparent = true,
+				-- fluo colour
+				fluo_color = "green",
+			})
+		end,
+	},
+	-- vague theme
+	{
+		"vague2k/vague.nvim",
+		config = function()
+		require("vague").setup({
+			-- make the background opaque
+			transparent = false,
+		})
+		end,
+	},
 }
